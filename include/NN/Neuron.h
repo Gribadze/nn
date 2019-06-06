88cad7d990c3273c622276f234372f44fb2d2fec
Dmitry Fiodorov <d.fiodorov@vironit.co.uk>
2019-06-06 17:15:09 +0300

    Neuron.h not abstract

--- include/NN/Neuron.h
+++ include/NN/Neuron.h
@@ -10,33 +10,31 @@
 
 class Neuron {
 private:
+    double m_input;
     double m_output;
     double m_derivedVal;
     const Activation::IActivation *m_activation;
-protected:
-    double m_input;
+public:
 
     explicit Neuron(
             double t_input = 0.0,
-            Activation::IActivation *t_activation = Activation::ActivationFactory::create(Activation::ActivationType::Sigmoid)
+            Activation::IActivation *t_activation = Activation::ActivationFactory::create(
+                    Activation::ActivationType::Sigmoid)
     );
 
     double getInput() const { return this->m_input; }
 
     double getOutput() const { return this->m_output; }
 
-public:
     Neuron(const Neuron &copy);
 
-    virtual ~Neuron();
+    ~Neuron();
 
-    virtual Neuron *clone() const = 0;
+    Neuron *clone() const;
 
-    virtual void setValue(double t_input);
+    void setValue(double t_input);
 
-    virtual double getValue() const = 0;
-
-    virtual void print() const;
+    void print() const;
 
     double getDerived() const { return this->m_derivedVal; }
 
